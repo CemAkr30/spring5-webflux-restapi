2d31f0730d111b775af41db1918a1d00e6e40d3f
CemAkr30 <cemakar18@gmail.com>
2023-02-18 00:30:59 +0300

    add

---
+++ src/main/java/ca/springframework/spring5webfluxrestapi/Spring5WebfluxRestapiApplication.java
@@ -0,0 +1,13 @@
+package ca.springframework.spring5webfluxrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebfluxRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebfluxRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/springframework/spring5webfluxrestapi/Spring5WebfluxRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.springframework.spring5webfluxrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5WebfluxRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
